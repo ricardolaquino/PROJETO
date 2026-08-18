06bad44591d29dec8c7d30eb56f0ca30c381fa35
ricardo.l.aquino@gmail.com <ricardo.l.aquino@gmail.com>
2026-08-17 23:15:42 -0300

    Organiza projeto e remove copias duplicadas

--- PROJETO 17/rapid-vision/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from "@tanstack/react-router";
-import { Route as RootRoute } from "./root";
-import PlantaEditor from "@/components/PlantaEditor";
-
-export const Route = createRoute({
-  getParentRoute: () => RootRoute,
-  path: "/",
-  component: PlantaEditor,
-});
